ab1ea3253c686d80cf8dd10ceb2c34b5aca2c8e5
Jonathan <j.d.old@lse.ac.uk>
2021-04-24 03:45:43 +0200

    Test

--- content/sections/gallery.md
+++
@@ -1,3 +0,0 @@
-
- *![Image](adma_backcover.jpg)
-
--- make.sh
+++ make.sh
@@ -2,3 +2,13 @@ cd /Users/jonathanold/Documents/GitHub/himani-arora-ha.github.io/
 rm -rf docs || exit 0
 hugo
 mv public docs
+ 
+ git remote remove origin
+ git remote add origin https://github.com/himani-arora-ha/himani-arora-ha.github.io.git
+
+ git add .
+git commit -m "Test"
+
+ git push -u origin source
+
+ 
