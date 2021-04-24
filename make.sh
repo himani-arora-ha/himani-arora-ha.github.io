b4c2b0ed2f38fc2e4063b263d6c1207ea6bd573e
Jonathan <j.d.old@lse.ac.uk>
2021-04-24 12:45:24 +0200

    weird files appeared

--- make.sh
+++ make.sh
@@ -2,15 +2,4 @@ cd /Users/jonathanold/Documents/GitHub/himani-arora-ha.github.io/
 rm -rf docs || exit 0
 hugo
 mv public docs
- 
-git init --bare
 
- git remote remove origin
- git remote add origin https://github.com/himani-arora-ha/himani-arora-ha.github.io.git
-
- git add .
-git commit -m "Test"
-
- git push origin source -f
-
- 
